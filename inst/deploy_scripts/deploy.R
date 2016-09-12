66d7708619b7ae3be5bd25ccaffa0724d2c91750
Thomas Rosendal <thomas.rosendal@sva.se>
2016-09-12 14:59:13 +0200

    path of source file

--- inst/deploy_scripts/deploy.R
+++ inst/deploy_scripts/deploy.R
@@ -1,6 +1,6 @@
 library(svamap)
 data(NUTS_20M)
-pts <- read_point_data("/media/ubuntu1/CWD.csv.csv")
+pts <- read_point_data("/media/t/Falkenrapporter/E16-036 Grundrapport.csv")
 polys <- svamap::match_to_county(pts, NUTS_20M, "NUTS_ID")
 path_to_data <- svamap::write_data(polys)
 svamap::write_page(path_to_data, "/media/ESS_webpages/CWD/", overwrite = TRUE, browse = FALSE)
