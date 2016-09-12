5c8ae95970263ec3ecf194e6a0033c80e972aeaf
Thomas Rosendal <thomas.rosendal@sva.se>
2016-09-12 14:25:35 +0200

    Added swedish to map

--- tests/test.R
+++ tests/test.R
@@ -1,7 +1,6 @@
-detach("package:svamap", unload=TRUE)
 library(svamap)
 data(NUTS_20M)
-pts <- svamap::read_point_data()
+pts <- read_point_data()
 polys <- svamap::match_to_county(pts, NUTS_20M, "NUTS_ID")
 path_to_data <- svamap::write_data(polys)
 svamap::write_page(path_to_data)
