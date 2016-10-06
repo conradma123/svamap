95908e918275f7b3020e8141f7f5b1b73b5e3524
Thomas Rosendal <thomas.rosendal@sva.se>
2016-10-06 17:45:46 +0200

    Added a third template

---
+++ inst/deploy_scripts/deploy3.R
@@ -0,0 +1,46 @@
+library(svamap)
+##
+data(NUTS_20M)
+##
+##Read in the point data
+########################
+pts <- read_point_data("/media/t/Falkenrapporter/E16-036 Grundrapport.csv")
+##pts <- read_point_data()
+##
+## Drop the points that are not "Vilt (Jakt - fiske - natur)"
+########################
+pts <- pts[pts$Djurhållning == "Vilt (Jakt - fiske - natur)",]
+##
+## Define the text to go in the popup for the point data
+########################
+pts$popup_text <- paste0(pts$Djurslag,
+                         "<br>Ankomstdatum: ", as.Date(pts$Ankomstdatum),
+                         "<br>Djuridentitet: ", pts@data$Djuridentitet)
+## Drop data that you don't want to display from points
+########################
+pts@data <- data.frame(species = pts@data$Djurslag,
+                       result = pts@data$Status..numerisk.,
+                       popup_text = pts@data$popup_text, stringsAsFactors = FALSE)
+##
+moose <- pts[pts@data$species == "Älg",]
+deer <- pts[pts@data$species == "Rådjur",]
+kronhjort <- pts[pts@data$species == "Kronhjort",]
+##Write data to geojson
+########################
+path_to_data <- svamap::write_data(list(pts, moose, deer, kronhjort))
+##
+##Deploy map
+########################
+svamap::write_page(data = path_to_data,
+                   path = "/media/ESS_webpages/CWD3/",
+                   template = "map3",
+                   overwrite = TRUE,
+                   browse = FALSE)
+##
+##test Deploy map
+########################
+## svamap::write_page(data = path_to_data,
+##                    path = "/tmp/",
+##                    template = "map2",
+##                    overwrite = TRUE,
+##                    browse = TRUE)
