c942e00525954c8fdee991b7c214655bd53d3a1e
Thomas Rosendal <thomas.rosendal@sva.se>
2016-10-10 10:41:29 +0200

    Bug

--- inst/deploy_scripts/deploy.R
+++ inst/deploy_scripts/deploy.R
@@ -1,7 +1,7 @@
 library(svamap)
 data(NUTS_20M)
 pts <- read_point_data("/media/t/Falkenrapporter/E16-036 Grundrapport.csv")
-polys <- svamap::match_to_county(pts, NUTS_20M, "NUTS_ID")
+polys <- svamap::match_to_county(pts, NUTS_20M, "NUTS_ID")$polygons
 text <- ifelse(polys$count == 1, "negativt prov", "negativa prover")
 polys$popup_text <- ifelse(is.na(polys$count), polys$name, paste0(polys$name, "<br>", polys$count, " ", text))
 path_to_data <- svamap::write_data(polys)
