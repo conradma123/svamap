3608d6c9334fd8c4742be5057d635450afc8c3fd
Thomas Rosendal <thomas.rosendal@sva.se>
2017-03-10 15:33:07 +0100

    Changes to norway map

--- inst/deploy_scripts/norway.R
+++ inst/deploy_scripts/norway.R
@@ -1,53 +1,76 @@
+library(svamap)
 ## Read data from website
 library(XML)
+## Read the kommune data:
 df <- readHTMLTable("http://www.hjorteviltregisteret.no/HelseInnsyn/StatistikkCWD", which = 1, stringsAsFactors = FALSE)
 ## Keep just kommune
 kommune <- df[trimws(df$Fylke) == "",]
-library(rgdal)
+## Get boundaries
+data(kommuner_norge, package = "svamap")
+## Define a colour scale
 colours <- c('#f7f7f7','#d9d9d9','#bdbdbd','#969696','#636363','#252525')
-## Get the map of norway kommune
-kommune_map <- readOGR("/media/trosendal/OS/projects/small_projects/CWD_norge/abas/kommuner.geojson",
-                       "OGRGeoJSON", stringsAsFactors = FALSE)
-data <- data.frame(kommune_map)
-## Simplify the geometry a little
-library(rmapshaper)
-kommune_map <- ms_simplify(kommune_map, keep_shapes = TRUE, keep = 0.01)
-kommune_map <- SpatialPolygonsDataFrame(kommune_map, data)
 ## match the data
-kommune_map@data$result <- kommune$Totalt[match(kommune_map@data$navn, kommune$Kommune)]
-kommune_map@data$positive <- kommune$Positive[match(kommune_map@data$navn, kommune$Kommune)]
+kommuner_norge@data$result <- kommune$Totalt[match(kommuner_norge@data$navn, kommune$Kommune)]
+kommuner_norge@data$positive <- kommune$Positive[match(kommuner_norge@data$navn, kommune$Kommune)]
 ## Assign colours by counts
-q <- quantile(as.numeric(kommune_map@data$result),
+q <- quantile(as.numeric(kommuner_norge@data$result),
               na.rm = TRUE,
               probs = c(0, 0.5, 0.6, 0.7, 0.8, 0.9,1))
-kommune_map@data$colour <- colours[as.numeric(as.character(cut(as.numeric(kommune_map@data$result),
+kommuner_norge@data$colour <- colours[as.numeric(as.character(cut(as.numeric(kommuner_norge@data$result),
                                                                q,
                                                                include.lowest = TRUE, labels = c(1:6))
                                                            )
                                               )]
-kommune_map$popup_text <- paste0(kommune_map$navn,
-                                 "<br>Total number tested: ", kommune_map@data$result,
-                                 "<br>Total number Positive: ", kommune_map@data$positive)
+kommuner_norge$popup_text <- paste0(kommuner_norge$navn,
+                                 "<br>Total number tested: ", kommuner_norge@data$result,
+                                 "<br>Total number Positive: ", kommuner_norge@data$positive)
 ## make positive kommuner red
-kommune_map@data$colour[which(as.numeric(kommune_map@data$positive)>0)] <- "#D22630"
-## Creat popup text
-kommune_map@data <- kommune_map@data[,names(kommune_map@data) %in% c("navn", "result", "colour", "popup_text")]
+kommuner_norge@data$colour[which(as.numeric(kommuner_norge@data$positive)>0)] <- "#D22630"
+## Create popup text
+kommuner_norge@data <- kommuner_norge@data[,names(kommuner_norge@data) %in% c("navn", "result", "colour", "popup_text")]
+##
+## Read the Villreinområde data:
+df <- readHTMLTable("http://www.hjorteviltregisteret.no/HelseInnsyn/StatistikkCWD", which = 2, stringsAsFactors = FALSE)
+names(df)[1] <- "name"
+vro <- df
+## Get boundaries:
+data(villreinomrader, package = "svamap")
+## match the data (There are 4 areas in the web data not represented
+## in the map data) Also forced to use a fuzzy match because the names
+## are alittle different
+index <- do.call("c",lapply(Villreinomrader_norge@data$Omradenavn, function(x) {
+    if(length(agrep(x, vro$name, max.distance = 0.1))) {
+        agrep(x, vro$name, max.distance = 0.1)
+    } else {
+        return(NA)
+    }
+}))
+Villreinomrader_norge@data$result <- vro$Totalt[index]
+Villreinomrader_norge@data$positive <- vro$Positive[index]
+## Assign colours by counts
+q <- quantile(as.numeric(Villreinomrader_norge@data$result),
+              na.rm = TRUE,
+              probs = c(0, 0.7, 0.75, 0.8, 0.85, 0.9,1))
+Villreinomrader_norge@data$colour <- colours[as.numeric(as.character(cut(as.numeric(Villreinomrader_norge@data$result),
+                                                               q,
+                                                               include.lowest = TRUE, labels = c(1:6))
+                                                           )
+                                                        )]
+Villreinomrader_norge@data$result[is.na(Villreinomrader_norge@data$result)] <- 0
+Villreinomrader_norge@data$positive[is.na(Villreinomrader_norge@data$positive)] <- 0
+
+Villreinomrader_norge$popup_text <- paste0(Villreinomrader_norge$Omradenavn,
+                                 "<br>Total number tested: ", Villreinomrader_norge@data$result,
+                                 "<br>Total number Positive: ", Villreinomrader_norge@data$positive)
+## make positive kommuner red
+Villreinomrader_norge@data$colour[which(as.numeric(Villreinomrader_norge@data$positive)>0)] <- "#D22630"
+## Create popup text
+Villreinomrader_norge@data <- Villreinomrader_norge@data[,names(Villreinomrader_norge@data) %in% c("Omradenavn", "result", "colour", "popup_text")]
+##
 ## plot map
-library(svamap)
-path_to_data <- svamap::write_data(list(kommune_map))
+path_to_data <- svamap::write_data(list(kommuner_norge, Villreinomrader_norge))
 svamap::write_page(data = path_to_data,
-                    path = "/media/ESS_webpages/Norway_CWD/",
-                    template = "Norway",
-                    overwrite = TRUE,
-                    browse = FALSE)
-
-#### Fylke is too big to be meaningful
-## fylke <- df[trimws(df$Fylke) != "",]
-## fylke_map <- readOGR("abas/fylker.geojson", "OGRGeoJSON", stringsAsFactors = FALSE)
-## fylke_map@data$result <- fylke$Totalt[match(fylke_map@data$navn, fylke$Fylke)]
-## q <- quantile(as.numeric(fylke_map@data$result), na.rm = TRUE, probs = c(0, 0.1, 0.25, 0.5, 0.75, 0.8,1))
-## fylke_map@data$colour <- colours[as.numeric(as.character(cut(as.numeric(fylke_map@data$result), q, include.lowest = TRUE, labels = c(1:6))))]
-## plot(fylke_map, col = fylke_map@data$colour)
-
-## Vilomraade we need a shapefile or some kind of spatial data
-## viltreinomraade <- readHTMLTable("http://www.hjorteviltregisteret.no/HelseInnsyn/StatistikkCWD", which = 2, stringsAsFactors = FALSE)
+##                  path = "/media/ESS_webpages/Norway_CWD/",
+                   owntemplate = "/home/trosendal/projects/small_projects/svamap/svamap/inst/Norway/map.html",
+                   overwrite = TRUE,
+                   browse = TRUE)
