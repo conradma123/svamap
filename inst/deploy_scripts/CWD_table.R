813c77ae41e647469a3ba2b4e14c615818e54444
Thomas Rosendal <thomas.rosendal@sva.se>
2017-04-25 12:08:03 +0200

    Simlified the CWD table by using new rough lan data

--- inst/deploy_scripts/CWD_table.R
+++ inst/deploy_scripts/CWD_table.R
@@ -3,10 +3,13 @@ library(rgeos)
 library(sp)
 library(RCurl)
 ##
-data(NUTS_20M)
+data(rough_lan)
 ##
-##Read in the point data
+##Read in the data
 ########################
+pts <- read.csv2("/media/t/Falkenrapporter/E16-036 Grundrapport.csv")
+## This is the data that should be tablulated
+
 pts <- read_point_data("/media/t/Falkenrapporter/E16-036 Grundrapport.csv")
 ##
 pts@data$Publicera <- factor(pts@data$Publicera, levels = c("Ja", "Nej"))
@@ -25,26 +28,16 @@ pts <- pts[pts$Djurhållning == "Vilt (Jakt - fiske - natur)" & !is.na(pts$Djurh
 ##Count points per polygon
 ########################
 ## Project to planar
-org_proj <- proj4string(NUTS_20M)
-counties <- spTransform(NUTS_20M, CRSobj = "+init=epsg:3021 +proj=tmerc +lat_0=0 +lon_0=15.80827777777778 +k=1 +x_0=1500000 +y_0=0 +ellps=bessel +towgs84=414.1,41.3,603.1,-0.855,2.141,-7.023,0 +units=m +no_defs")
-## Put a buffer to catch the points on the perimeter: This is
-## suboptimal since an overlapping internal boundaries are created in
-## the gBuffer function
-temp <- gBuffer(counties, byid = TRUE, width = 10000)
-counties <- spTransform(counties, org_proj)
-temp <- spTransform(temp, org_proj)
-polys <- svamap::match_to_county(pts, counties, "NUTS_ID")
-polys2 <- svamap::match_to_county(polys$outlier_pts, temp, "NUTS_ID")
-polys$polygons@data$count[is.na(polys$polygons@data$count)] <- 0
-polys2$polygons@data$count[is.na(polys2$polygons@data$count)] <- 0
-## Add the perimeter points to the county count
-polys$polygons@data$count <- polys$polygons@data$count + polys2$polygons@data$count
-polys <- polys$polygons
+polys <- svamap::match_to_county(pts, rough_lan, "NUTS_ID")
+polys <- polys[[1]]
 ## Just keep the basic info for the table
 df <- polys@data[,c("name", "count")]
 df$count <- as.integer(df$count)
+df$count[is.na(df$count)] <- 0
 total <- sum(df$count)
 df <- rbind(df, c("Total", total))
+
+
 ## write the table
 nolink <- c("<META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'>")
 tab <- html_table(df,
