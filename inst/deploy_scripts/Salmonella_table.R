c34a8f83368b2a96ea6ac5b84b893500ac16724e
Thomas Rosendal <thomas.rosendal@sva.se>
2017-05-24 08:47:07 +0200

    Draft of Table
    @lernholm HAve a look at this script of a table

---
+++ inst/deploy_scripts/Salmonella_table.R
@@ -0,0 +1,40 @@
+library(svamap)
+library(readxl)
+##
+##Read in the data from URAX
+########################
+##df <- read.csv2("/media/t/Falkenrapporter/urax.csv", stringsAsFactor = FALSE)
+##df <- df[df$Namn == "Salmonella Feed 2017",]
+##df <- df[!duplicated(df$Namn.1),]
+##table(df$Namn.1, df$Status)
+
+## Seems there are not the data we need there (No "Djurslag" or "provtagna"):
+
+## Read the data
+df <- read_xlsx("/media/i/ESS/EPIZ/test/Salmonella_foder_2017_tabell.xlsx")
+## Tablulate the results
+df <- as.data.frame.matrix(table(df$Djurslag, df$Status))
+## Create a variable
+df$Provtagna <- df$Neg + df$Pos + df$Pågåande
+## Move the rownames into a variable
+df$Djurslag <- rownames(df)
+## Order the columns neatly
+df <- df[,c("Djurslag", "Provtagna", "Pågåande", "Neg", "Pos")]
+## write the table
+nolink <- c("<META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'>")
+tab <- html_table(df,
+                  align = c("l", "l", "l", "l", "l"),
+                  html_head = generate_header(ordering =FALSE, otherstuff = nolink),
+                  footer = FALSE
+                  )
+## Browse the table that we made
+browseURL(tab)
+
+## The following lines will be used when we want to deploy the table to the web:
+##
+## Deploy map to Azure server. This is SVA's external website and is
+## administered by a company "Episerver hosting" the contact for this
+## company at SVA is the communications department.
+## temp <- readLines("~/.svaftp_credentials")
+## cred <- paste0("ftp://", temp[2], ":", temp[3], "@", temp[1], "/MAPS/CWD/")
+## ftpUpload(tab, paste0(cred, "table.html"))
